2c145e31babeea5679a2855a78c3ebd5fb065c0b
Esdes74 <115540633+Esdes74@users.noreply.github.com>
2023-05-08 12:33:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eslamber <eslamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 17:21:42 by eslamber          #+#    #+#             */
-/*   Updated: 2023/05/04 17:29:26 by eslamber         ###   ########.fr       */
+/*   Updated: 2023/05/08 12:33:24 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,29 @@
 
 int	main(int ac, char **av, char **env)
 {
-	if (ac > 1)
-		if (execve(av[1], av + 1, env) == -1)
-			ft_printf("it doesn't work\n");
+	int	id;
+	int	outin[2];
+	int	infile;
+	int	outfile;
+
+	if (pipe(outin) == -1)
+		ft_printf("Error : There is a probleme with the pipe system\n");
+	id = fork();
+	if (ac == 5)
+	{
+		if (id == 0) // child
+		{
+			infile = open(av[1], O_RDONLY);
+			if (infile == -1)
+				ft_printf("Error : Wrong opening infile.\n");
+			dup2(); // TODO :
+		}
+		else if (id > 0) // parent
+						 // TODO :
+		else
+			ft_printf("Error : There is a problem with the fork system.\n");
+	}
+	else
+		ft_printf("Error : You don't have enought parameters.\n");
 	return (0);
 }
