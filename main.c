61df0ff648e565056a93111f66b07aa504519877
Esdes74 <115540633+Esdes74@users.noreply.github.com>
2023-06-03 15:44:39 +0200

    normé

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eslamber <eslamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 17:21:42 by eslamber          #+#    #+#             */
-/*   Updated: 2023/06/03 15:06:16 by eslamber         ###   ########.fr       */
+/*   Updated: 2023/06/03 15:25:24 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ static char	*cmd_build(char *str)
 	size_t	j;
 	size_t	size;
 
-	/* if (str[0] == '/' && str[1] == 'b' && str[2] == 'i' && str[3] == 'n' */
-	/* 		&& str[4] == '/') */
 	if (ft_in('/', str) == 1)
 		return (str);
 	new = NULL;
@@ -39,7 +37,6 @@ static char	*cmd_build(char *str)
 	while (j < ft_strlen(str))
 		new[i++] = str[j++];
 	new[i] = '\0';
-	/* ft_printf("new = %s, i = %d\n", new, i); */
 	return (new);
 }
 
@@ -53,77 +50,79 @@ static void	anihilation(char **splitted)
 	free(splitted);
 }
 
+static int	child(int outin[2], char **av, char **environ)
+{
+	char	*cmd;
+	char	**splitted;
+	int		infile;
+
+	close(outin[0]);
+	infile = open(av[1], O_RDONLY);
+	if (infile == -1)
+		ft_printf("Error : Wrong opening infile.\n");
+	if (dup2(infile, STDIN_FILENO) == -1)
+		ft_printf("Error : Probleme with first dup programm in child\n");
+	if (dup2(outin[1], STDOUT_FILENO) == -1)
+		ft_printf("Error : Probleme with second dup programm in child\n");
+	close(infile);
+	splitted = ft_split(av[2], ' ');
+	if (splitted == NULL)
+		return (1);
+	cmd = cmd_build(splitted[0]);
+	if (cmd == NULL)
+		return (free(splitted), 1);
+	if (execve(cmd, splitted, environ) == -1)
+		perror("Child : command not found");
+	return (free(cmd), anihilation(splitted), 0);
+}
+
+static int	parent(int outin[2], char **av, char **environ)
+{
+	char	*cmd;
+	char	**splitted;
+	int		outfile;
+
+	wait(NULL);
+	close(outin[1]);
+	outfile = open(av[4], O_CREAT | O_RDWR | O_TRUNC, S_IRUSR + S_IWUSR + \
+			S_IRGRP + S_IROTH);
+	if (outfile < 0)
+		ft_printf("Error : Wrong opening outfile.\n");
+	if (dup2(outin[0], STDIN_FILENO) == -1)
+		ft_printf("Error : Problem with first dup programme in parent.\n");
+	if (dup2(outfile, STDOUT_FILENO) == -1)
+		ft_printf("Error : Problem with second dup programme in parent.\n");
+	close(outfile);
+	splitted = ft_split(av[3], ' ');
+	if (splitted == NULL)
+		return (1);
+	cmd = cmd_build(splitted[0]);
+	if (cmd == NULL)
+		return (free(splitted), 1);
+	if (execve(cmd, splitted, environ) == -1)
+		perror("Parent : command not found");
+	return (free(cmd), anihilation(splitted), 0);
+}
+
 int	main(int ac, char **av, char *environ[])
 {
 	int		id;
 	int		outin[2];
-	int		infile;
-	int		outfile;
-	char	*cmd;
-	char	**splitted;
-	/* int	i = 0; */
 
 	if (ac == 5 && environ != NULL)
 	{
 		if (pipe(outin) == -1)
 			ft_printf("Error : There is a probleme with the pipe system.\n");
 		id = fork();
-		if (id == 0) // child
+		if (id == 0)
 		{
-			close(outin[0]);
-			infile = open(av[1], O_RDONLY);
-			if (infile == -1)
-				ft_printf("Error : Wrong opening infile.\n");
-			if (dup2(infile, STDIN_FILENO) == -1)
-				ft_printf("Error : Probleme with first dup programm in child\n");
-			if (dup2(outin[1], STDOUT_FILENO) == -1)
-				ft_printf("Error : Probleme with second dup programm in child\n");
-			close(infile);
-			splitted = ft_split(av[2], ' ');
-			if (splitted == NULL)
+			if (child(outin, av, environ) == 1)
 				return (1);
-			cmd = cmd_build(splitted[0]);
-			if (cmd == NULL)
-				return (free(splitted), 1);
-			/* while (test[i][0] != '\0') */
-			/* 	i++; */
-			/* free(test[i]); */
-			/* test[i] = NULL; */
-			if (execve(cmd, splitted, environ) == -1)
-				perror("Child : command not found");
-				/* ft_printf("Error : Wrong child execution.\n"); */
-			free(cmd);
-			anihilation(splitted);
 		}
-		else if (id > 0) // parent
+		else if (id > 0)
 		{
-			wait(NULL);
-			close(outin[1]);
-			outfile = open(av[4], O_CREAT | O_RDWR | O_TRUNC, S_IRUSR + S_IWUSR + S_IRGRP + S_IROTH);
-			if (outfile < 0)
-				ft_printf("Error : Wrong opening outfile.\n");
-			if (dup2(outin[0], STDIN_FILENO) == -1)
-				ft_printf("Error : Problem with first dup programme in parent.\n");
-			if (dup2(outfile, STDOUT_FILENO) == -1)
-				ft_printf("Error : Problem with second dup programme in parent.\n");
-			close(outfile);
-			splitted = ft_split(av[3], ' ');
-			if (splitted == NULL)
+			if (parent(outin, av, environ) == 1)
 				return (1);
-			cmd = cmd_build(splitted[0]);
-			if (cmd == NULL)
-				return (free(splitted), 1);
-			/* i = 0; */
-			/* while (test[i] != NULL) */
-			/* 	i++; */
-			/* free(test[i]); */
-			/* test[i] = NULL; */
-			/* ft_printf("cmd = %s\n", cmd); */
-			if (execve(cmd, splitted, environ) == -1)
-				perror("Parent : command not found");
-				/* ft_printf("Error : Wrong parent execution.\n"); */
-			free(cmd);
-			anihilation(splitted);
 		}
 		else
 			ft_printf("Error : There is a problem with the fork system.\n");
